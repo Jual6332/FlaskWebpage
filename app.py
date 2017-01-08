ef3f20f80374b1e3e6c3e2aee80a3a29dd9d62b1
Jual6332 <jual6332@colorado.edu>
2017-01-07 19:49:17 -0700

    Added login wraps and loggedin_required

--- app.py
+++ app.py
@@ -1,9 +1,24 @@
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask, render_template, redirect, url_for, request, session, flash
+from functools import wraps
+
 app = Flask(__name__)
 
+app.secret_key = "my precious"
+
+def login_required(f):
+	@wraps(f)
+	def wrap(*args, **kwargs):
+		if 'logged_in' in session:
+			return f(*args, **kwargs)
+		else:
+			flash('You need to login first.')
+			return redirect(url_for('login'))
+	return wrap
+
 @app.route('/')
+@login_required
 def home():
-	return "<strong>Hello World, new Flask App</strong>"
+	return render_template('index.html')
 
 @app.route('/welcome')
 def welcome():
@@ -16,8 +31,17 @@ def login():
 		if request.form['username'] != 'admin' or request.form['password'] != 'admin':
 			error = 'Invalid credentials. Please try again.'
 		else:
+			session['logged_in'] = True
+			flash('You were just logged in!')
 			return redirect(url_for('home'))
 	return render_template('login.html',error=error)
+
+@app.route('/logout')
+@login_required
+def logout():
+	session.pop('logged_in', None)
+	flash('You were just logged out!')
+	return redirect(url_for('welcome'))
 
 if __name__ == '__main__':
 	app.run(debug=True)
